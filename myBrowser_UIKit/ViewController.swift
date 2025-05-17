f48a2b80642bfd65a9a4d26d951c5aa4f2efa400
rahulsingh1101 <rkssingh566@gmail.com>
2025-05-17 10:04:32 +0530

    Initial Commit

---
+++ myBrowser_UIKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  myBrowser_UIKit
+//
+//  Created by Rahul Singh on 17/05/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
