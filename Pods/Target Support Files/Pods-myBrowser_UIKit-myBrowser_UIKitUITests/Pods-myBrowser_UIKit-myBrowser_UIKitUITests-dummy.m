0292b41a2c104a0a5d7608fc16fc99a3ee7d53e3
rahulsingh1101 <rkssingh566@gmail.com>
2025-05-27 23:32:48 +0530

    removed pod files from GITHub

--- Pods/Target Support Files/Pods-myBrowser_UIKit-myBrowser_UIKitUITests/Pods-myBrowser_UIKit-myBrowser_UIKitUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myBrowser_UIKit_myBrowser_UIKitUITests : NSObject
-@end
-@implementation PodsDummy_Pods_myBrowser_UIKit_myBrowser_UIKitUITests
-@end
--- Pods/Target Support Files/Pods-myBrowser_UIKit/Pods-myBrowser_UIKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myBrowser_UIKit : NSObject
-@end
-@implementation PodsDummy_Pods_myBrowser_UIKit
-@end
