c59e9b15adf557a3ecc2e2989c4a54fef8eea46a
Rahul <rkssingh566@gmail.com>
2025-09-03 08:45:48 +0530

    removed un wanted code and targets

--- myBrowser_UIKit/PreloadWindow/TaskListController/Color+String.swift
+++ myBrowser_UIKit/PreloadWindow/TaskListController/Color+String.swift
@@ -29,24 +29,3 @@ extension String {
         }
     }
 }
-
-extension Priority {
-    func priorityColour() -> Color {
-        switch self {
-        case .P0:
-            Color(red: 225/255, green: 45/255, blue: 69/255)
-        case .P1:
-            Color(red: 255/255, green: 111/255, blue: 97/255)
-        case .P2:
-            Color(red: 255/255, green: 187/255, blue: 60/255)
-        case .P3:
-            Color(red: 0/255, green: 168/255, blue: 107/255)
-        case .P4:
-            Color(red: 64/255, green: 156/255, blue: 255/255)
-        case .P5:
-            Color(red: 178/255, green: 190/255, blue: 195/255)
-        case .no_value:
-            Color(.brown)
-        }
-    }
-}
